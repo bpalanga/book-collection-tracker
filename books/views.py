90be53ac518d080d173c2764bf50cfcdbd6c02cc
bpalanga <152504993+bpalanga@users.noreply.github.com>
2025-01-10 19:11:56 +0200

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# View to list all the books
+def book_list(request):
+    return HttpResponse("This is the list of books.")
+
+# View to display details of one book
+def book_details(request, book_id):
+    return HttpResponse(f"This is the detail view for book ID: {book_id}")
+
+# View to add a book
+def add_book(request):
+    return HttpResponse("This is the form to add a new book.")
+  
+
 
 # Create your views here.
